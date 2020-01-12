68e4506482bff951adaf85972c98bd5c841ad018
tihangrai <tihangrai@gmail.com>
2020-01-11 23:39:33 -0500

    first commit

---
+++ app/Controllers/Http/CustomerController.js
@@ -0,0 +1,106 @@
+'use strict'
+
+/** @typedef {import('@adonisjs/framework/src/Request')} Request */
+/** @typedef {import('@adonisjs/framework/src/Response')} Response */
+/** @typedef {import('@adonisjs/framework/src/View')} View */
+
+const Customer = use('App/Models/Customer')
+
+/**
+ * Resourceful controller for interacting with customers
+ */
+class CustomerController {
+  /**
+   * Show a list of all customers.
+   * GET customers
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   * @param {View} ctx.view
+   */
+  async index ({ request, response, view }) {
+
+      const customers = await Customer.all();
+      response.json(customers)
+
+  }
+
+  /**
+   * Render a form to be used for creating a new customer.
+   * GET customers/create
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   * @param {View} ctx.view
+   */
+  async create ({ request, response, view }) {
+  }
+
+  /**
+   * Create/save a new customer.
+   * POST customers
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   */
+  async store ({ request, response }) {
+
+    const data = request.only(['name', 'description'])
+    const user = await Customer.create(data);
+
+    response.json({
+      'message' : 'User successfully created',
+    })
+  }
+
+  /**
+   * Display a single customer.
+   * GET customers/:id
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   * @param {View} ctx.view
+   */
+  async show ({ params, request, response, view }) {
+  }
+
+  /**
+   * Render a form to update an existing customer.
+   * GET customers/:id/edit
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   * @param {View} ctx.view
+   */
+  async edit ({ params, request, response, view }) {
+  }
+
+  /**
+   * Update customer details.
+   * PUT or PATCH customers/:id
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   */
+  async update ({ params, request, response }) {
+  }
+
+  /**
+   * Delete a customer with id.
+   * DELETE customers/:id
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   */
+  async destroy ({ params, request, response }) {
+  }
+}
+
+module.exports = CustomerController
